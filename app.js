5419da29b169dae26fabab91c8b9be9aebce6230
Diamondw69 <86194579+Diamondw69@users.noreply.github.com>
2022-05-18 23:35:44 +0600

    make it better

--- app.js
+++ app.js
@@ -21,12 +21,12 @@ schema
     .has().not().spaces()
     .has().symbols();
 
-mongoose.connect('mongodb://localhost/restBook_db', {
+mongoose.connect('mongodb://localhost:27017/restBook_db', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     family:4
 })
-let mongoClient = new mongodb.MongoClient('mongodb://localhost/', {
+let mongoClient = new mongodb.MongoClient('mongodb://localhost:27017/', {
     useUnifiedTopology: true
 });
 let app= express()
